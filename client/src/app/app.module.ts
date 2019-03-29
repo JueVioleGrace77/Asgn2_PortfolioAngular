af482e75c3ebdbfcd6580bc9c61255295d804fa9
Harvey <crazyapple077@yahoo.com>
2019-03-29 02:07:05 -0400

    Angular Client side/API Routing

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,11 +1,7 @@
-// Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
 
-// Components
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './partials/header/header.component';
 import { FooterComponent } from './partials/footer/footer.component';
@@ -16,23 +12,6 @@ import { ProductsComponent } from './pages/products/products.component';
 import { ServicesComponent } from './pages/services/services.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
-import { ContactListComponent } from './contacts/contact-list/contact-list.component';
-import { RegisterComponent } from './pages/register/register.component';
-import { LoginComponent } from './pages/login/login.component';
-import { ContactDetailsComponent } from './contacts/contact-details/contact-details.component';
-import { ContactDeleteComponent } from './contacts/contact-delete/contact-delete.component';
-
-// Services
-import { FlashMessagesModule, FlashMessagesService } from 'angular2-flash-messages';
-import { AuthService } from './services/auth.service';
-import { JwtModule, JwtHelperService, JwtInterceptor } from '@auth0/angular-jwt';
-
-// Route Guards
-import { AuthGuard } from './guards/auth.guard';
-
-export function jwtTokenGetter() {
-  return localStorage.getItem('id_token');
-}
 
 @NgModule({
   declarations: [
@@ -45,26 +24,13 @@ export function jwtTokenGetter() {
     ProductsComponent,
     ServicesComponent,
     ContactComponent,
-    PageNotFoundComponent,
-    ContactListComponent,
-    RegisterComponent,
-    LoginComponent,
-    ContactDetailsComponent,
-    ContactDeleteComponent
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule,
-    FlashMessagesModule,
-    JwtModule.forRoot({
-      config: {
-        tokenGetter: jwtTokenGetter
-      }
-    })
+    AppRoutingModule
   ],
-  providers: [FlashMessagesService, AuthGuard],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
