364566c06acc9537d0e38d28c46d30e826759de1
Harvey <crazyapple077@yahoo.com>
2019-03-30 21:52:02 -0400

    built for heroku deployment

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "URI" : "mongodb://localhost/test",
-     //"URI" : "mongodb://thomas:a123456@ds060977.mlab.com:60977/comp308-w2019-lesson4b"
-     "secret": "SomeSecret"
+    //"URI" : "mongodb://localhost/test",
+    "URI" : "mongodb://harvey:password1@ds050739.mlab.com:50739/comp308-w2019-portfolio-angular",
+    "secret": "SomeSecret"
  }
